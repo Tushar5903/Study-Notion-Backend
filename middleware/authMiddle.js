4270bbbeacc43500d8af50b7d45ce679691ee550
Tushar5903 <164053903+Tushar5903@users.noreply.github.com>
2025-06-30 13:22:13 +0530

    Fix: updated file casing and corrected imports

--- middleware/authMiddle.js
+++ middleware/authMiddle.js
@@ -32,7 +32,7 @@ exports.auth = async (req, res, next) => {
 
     next();
   } catch (error) {
-    return res.status(401).json({
+    return res.status(500).json({
       success: false,
       message: "Something went wrong when validating the token",
     });
