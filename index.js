4066fcbb37e4dd03d2c6f91959515860ea2f2ada
Tushar5903 <164053903+Tushar5903@users.noreply.github.com>
2025-07-05 12:10:25 +0530

    Fix: updated file casing and corrected imports

--- index.js
+++ index.js
@@ -33,29 +33,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://studynotion-by-tushar-i9z098muu-tushar-kumars-projects-f26e856d.vercel.app", 
-];
+
 
 app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || allowedOrigins[0]);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+	cors({
+		origin:"http://localhost:3000",
+		credentials:true,
+	})
+)
 
 
 
